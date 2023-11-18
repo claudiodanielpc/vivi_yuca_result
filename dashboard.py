cdb209ac0202574d0fbf7a894da3af58cade6e6f
Claudio Daniel Pacheco Castro <claudio@comunidad.unam.mx>
2023-11-17 21:03:30 -0600

    Update

--- dashboard.py
+++ dashboard.py
@@ -21,10 +21,14 @@ from sqlalchemy import create_engine
 # # Invoke the table into a pandas DataFrame
 # df = pd.read_sql_table(table_name, engine)
 
-
-df
+df=pd.read_csv('vivienda_yucatan.csv')
 
 
 st.set_page_config(page_title="Resultados vivienda en Mérida", page_icon=":house:")
 
 st.markdown("<p style='font-family: Montserrat; font-weight: bold;font-size: 35px; text-align: center'>Portales inmobiliarios y oferta disponible de vivienda nueva en Mérida</p>", unsafe_allow_html=True)
+
+
+#Histograma de precios utilizando plotly express
+st.markdown("<p style='font-family: Montserrat; font-weight: bold;font-size: 25px; text-align: center'>Histograma de precios</p>", unsafe_allow_html=True)
+fig = px.histogram(df, x="precio", nbins=100, title='Histograma de precios')
