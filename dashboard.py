8b5299a7d3f403092d20085215b3b03d86fe3926
Claudio Daniel Pacheco Castro <claudio@comunidad.unam.mx>
2023-11-17 21:02:15 -0600

    tabla

--- dashboard.py
+++ dashboard.py
@@ -9,18 +9,20 @@ import requests
 from IPython.display import IFrame
 from sqlalchemy import create_engine
 
-username = 'postgres'
-password = 'olivia14'
+# username = 'postgres'
+# password = 'olivia14'
 
-# Setting up the connection string for SQLAlchemy
-engine = create_engine(f'postgresql://{username}:{password}@localhost:5432/postgres')
+# # Setting up the connection string for SQLAlchemy
+# engine = create_engine(f'postgresql://{username}:{password}@localhost:5432/postgres')
 
-# Replace 'table_name' with the actual name of your table
-table_name = 'vivienda_yucatan'
+# # Replace 'table_name' with the actual name of your table
+# table_name = 'vivienda_yucatan'
 
-# Invoke the table into a pandas DataFrame
-df = pd.read_sql_table(table_name, engine)
+# # Invoke the table into a pandas DataFrame
+# df = pd.read_sql_table(table_name, engine)
 
+
+df
 
 
 st.set_page_config(page_title="Resultados vivienda en Mérida", page_icon=":house:")
