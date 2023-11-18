12655b9f2d8381ededa62fdb9123972dbcc51fab
Claudio Daniel Pacheco Castro <claudio@comunidad.unam.mx>
2023-11-17 20:30:26 -0600

    se invoca tabla

--- dashboard.py
+++ dashboard.py
@@ -8,9 +8,19 @@ from streamlit_extras.dataframe_explorer import dataframe_explorer
 from streamlit.elements import spinner
 import requests
 from IPython.display import IFrame
-import sqlalchemy
+from sqlalchemy import engine
 
+username = 'postgres'
+password = 'olivia14'
 
+# Setting up the connection string for SQLAlchemy
+engine = create_engine(f'postgresql://{username}:{password}@localhost:5432/postgres')
+
+# Replace 'table_name' with the actual name of your table
+table_name = 'vivienda_yucatan'
+
+# Invoke the table into a pandas DataFrame
+df = pd.read_sql_table(table_name, engine)
 
 
 
