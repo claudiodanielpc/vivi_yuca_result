bb2a77caa1db53631c8967ba0eabd62d4264ee46
Claudio Daniel Pacheco Castro <claudio@comunidad.unam.mx>
2023-11-17 21:05:59 -0600

    update

--- dashboard.py
+++ dashboard.py
@@ -31,6 +31,6 @@ st.markdown("<p style='font-family: Montserrat; font-weight: bold;font-size: 35p
 
 #Histograma de precios utilizando plotly express
 st.markdown("<p style='font-family: Montserrat; font-weight: bold;font-size: 25px; text-align: center'>Histograma de precios</p>", unsafe_allow_html=True)
-fig = px.histogram(df, x="precio", nbins=100, title='Histograma de precios')
-st.plotly_chart(fig)
+fig = px.histogram(df, x='precio', nbins=10, title='Histograma de precios')
+fig.show()
 
